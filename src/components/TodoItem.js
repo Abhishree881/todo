958c2e1b93789806ac92a6c37a3f0db1afc22e5f
Abhishree <abhishree881@gmail.com>
2022-12-08 22:10:50 +0530

    improved styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import '../styling/Todo.css';
+import React from "react";
+import "../styling/Todo.css";
 
-export default function TodoItem({ todo,onComplete }) {
+export default function TodoItem({ todo, onComplete }) {
   return (
-    <div className='todo'>
+    <div className="todo">
       <h4>{todo.title}</h4>
       <h5>{todo.desc}</h5>
-      <div className='completed' onClick={() => onComplete(todo)}>Complete</div>
+      <div className="completed" onClick={() => onComplete(todo)}>
+        Complete
+      </div>
     </div>
-  )
+  );
 }
