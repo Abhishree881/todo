8d0044a987326840137c676f7be7c8d4649cca14
Abhishree <abhishree881@gmail.com>
2022-12-19 20:42:13 +0530

    added appraisals

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styling/Todo.css";
+import swal from "sweetalert";
 
 export default function TodoItem({ todo, onComplete }) {
   return (
@@ -7,7 +8,13 @@ export default function TodoItem({ todo, onComplete }) {
       <h1>Task : {todo.title}</h1>
       <h2>Description : {todo.desc}</h2>
       <h2>Due Date : {todo.time}</h2>
-      <div className="completed" onClick={() => onComplete(todo)}>
+      <div
+        className="completed"
+        onClick={() => {
+          onComplete(todo);
+          swal("Good Work", "You have completed your task", "success");
+        }}
+      >
         Complete
       </div>
     </div>
