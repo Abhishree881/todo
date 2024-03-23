729cf5507096772b4ec28b63c1a4ca2d821f89e8
Abhishree881 <abhishree881@gmail.com>
2024-03-23 12:20:30 +0530

    removed private api keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQU_FCA_JnumrLlBx9LjBcBeNPbRBMsgI",
-  authDomain: "todo-21922.firebaseapp.com",
-  projectId: "todo-21922",
-  storageBucket: "todo-21922.appspot.com",
-  messagingSenderId: "520268422582",
-  appId: "1:520268422582:web:a94349f7ffb312339e3517",
-  measurementId: "G-T8TD9KMMYS",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
